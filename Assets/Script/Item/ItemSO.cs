8fcc9b8a1235712643b0e7aaf800b5c084f35374
RRoroan <tjdals8788@gmail.com>
2025-03-27 20:43:02 +0900

    [Feat] ItemSO Inspector edit

--- Assets/Script/Item/ItemSO.cs
+++ Assets/Script/Item/ItemSO.cs
@@ -14,20 +14,23 @@ public class ItemSO : ScriptableObject, IItem
     [SerializeField] private GameObject itemPrefab;
 
     public string ItemName => itemName;
-    public void UseItem()
+    public void UseItem()//PlayerController player
     {
         switch (itemType)
         {
             case EItemType.Attack:
                 //TODO: 플레이어 공격
+                //player.UpdateStats(value)
                 break;
 
             case EItemType.Defense:
                 //TODO: 플레이어 방어
+                //player.UpdateStats(value)
                 break;
 
             case EItemType.Heal:
                 //TODO: 플레이어 회복
+                //player.UpdateHealth(value)
                 break;
         }
     }
