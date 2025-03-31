b6ee168a070f4416e570f8da22c8400b4d39929b
RRoroan <tjdals8788@gmail.com>
2025-03-31 18:13:58 +0900

    [Add] ItemSO item data

--- Assets/Script/Scriptable/ItemSO.cs
+++ Assets/Script/Scriptable/ItemSO.cs
@@ -14,6 +14,14 @@ public class ItemSO : ScriptableObject, IItem
     [SerializeField] private GameObject itemPrefab;
     [SerializeField] public bool stackable = true;
 
+    public ItemSO(string name, Sprite icon, bool stackable, int value)
+    {
+        this.itemName = name;
+        this.sprite = icon;
+        this.stackable = stackable;
+        this.value = value;
+    }
+
     public string ItemName => itemName;
     public void UseItem(Player player)//PlayerController player
     {
