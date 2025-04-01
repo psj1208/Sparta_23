6627ab20a9cb4b088a90a89ec965a59adb8f7dd5
RRoroan <tjdals8788@gmail.com>
2025-04-01 17:47:04 +0900

    [Fix] Spawn Item after 1 cycle

--- Assets/Script/Manager/ItemInventoryManager.cs
+++ Assets/Script/Manager/ItemInventoryManager.cs
@@ -22,6 +22,7 @@ public class ItemInventoryManager : Singleton<ItemInventoryManager>
     
     public void InitializeInventory()
     {
+        Debug.Log("Initialize");
         uiMain = UIManager.Get<UIMain>();
         UpdateInventoryUI();
         OnInventoryInitialized?.Invoke();
