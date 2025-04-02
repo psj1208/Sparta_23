d1bb24598f1ef6e031a5f5ca33d5af9c79b86613
ProgramCnt <corfafa2@naver.com>
2025-04-02 16:09:14 +0900

    [chore] 시작캔버스 생성 후 폴더정리

---
+++ Assets/Script/UI/StartCanvas.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartCanvas : CanvasBase<StartCanvas>
+{
+    private IEnumerator Start()
+    {
+        var oper = SceneManager.LoadSceneAsync("DontDestroy", LoadSceneMode.Additive);
+        yield return oper;
+        if (parents.Count > 0)
+            UIManager.SetParents(parents);
+        UIManager.Show<UIStart>();
+    }
+}
